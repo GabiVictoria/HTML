c0e781d6928863ae784fb22dee82a9baf05627f6
Victória <126773570+GabiVictoria@users.noreply.github.com>
2024-02-28 11:13:50 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./Components/header/header.jsx"
+import Main from "./Components/Main/main.jsx"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Main/main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./main.css"
+
+function Main(){
+    return(
+       <main>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas placeat illo, est commodi fuga impedit. Deleniti rem ut, excepturi nam, alias itaque beatae suscipit saepe laudantium aperiam quisquam natus explicabo.</p>
+       </main>
+    )
+}
+
+export default Main
